eda52eb256fc416bfe78ef7c666301d643f09b67
Ankush Rana <ankush.rana@bsc.es>
2024-03-25 14:51:09 +0100

    update main.py to make reloadable api

--- main.py
+++ main.py
@@ -3,18 +3,23 @@ import argparse
 import uvicorn
 from app import create_app
 
-if __name__ == "__main__":
+
+def get_arguments():
     parser = argparse.ArgumentParser(description="An API designed to provide translation services for text between different languages.")
     parser.add_argument("-m", "--models", type=str, default="./models", help="Directory path of models", required=False)
     parser.add_argument("-l", "--load", type=str, nargs="+", help="Option to load models, if it contains 'all' it will download all models", default=["es-ca", "ca-es"])
+    parser.add_argument("-r", "--reload", type=bool, help="Reload api on changes", action=argparse.BooleanOptionalAction)
+    parser.add_argument("-w", "--workers", type=int, help="Number of workers to run the api", default=None)
 
-    args = parser.parse_args()
+    return parser
+
+
+args = get_arguments().parse_args()
+
+if args.models:
     os.environ['MODELS_ROOT'] = args.models
-    models_to_load = args.load
-    if 'all' in models_to_load:
-        load_all_models = True
-    else:
-        load_all_models = False
 
-    app = create_app(load_all_models, models_to_load)
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_config = "logging.yml")
+app = create_app(args.load)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, log_config = "logging.yml", reload=args.reload, workers=args.workers)
