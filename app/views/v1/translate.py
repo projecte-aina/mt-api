9415b74ad77494ff8fd464115904707bd12bec40
Ankush Rana <ankush.rana@bsc.es>
2024-11-06 18:24:27 +0100

    fix bug to use_multi ignored

--- app/views/v1/translate.py
+++ app/views/v1/translate.py
@@ -15,8 +15,8 @@ from app.constants import MULTIMODALCODE
 
 translate_v1 = APIRouter(prefix='/api/v1/translate')
 
-DEVDEBUG = True
 logger = logging.getLogger('console_logger')
+logger.setLevel(logging.INFO)
 
 def fetch_model_data_from_request(request):
     config = Config()
@@ -39,10 +39,9 @@ def fetch_model_data_from_request(request):
                 detail=f'Language pair {model_id} is not supported.',
             )
 
-    if DEVDEBUG: 
-        logger.debug(f'compatible_model_ids {compatible_model_ids}')
-        if use_multi:
-            logger.debug(f'use_multi {use_multi}')
+    logger.debug(f'compatible_model_ids {compatible_model_ids}')
+    if use_multi:
+        logger.debug(f'use_multi {use_multi}')
     
     regular_model_exists = model_id in config.loaded_models
     multilingual_model_exists_for_pair = any([mid.startswith(MULTIMODALCODE) for mid in compatible_model_ids])
@@ -56,9 +55,10 @@ def fetch_model_data_from_request(request):
             # model_id = get_model_id(src=MULTIMODALCODE,
                                     # tgt=MULTIMODALCODE,
                                     # alt_id=request.alt)
-            model_id = config._pair_to_model_id(compatible_model_ids[0])
-            if len(compatible_model_ids) > 1:
-                logger.warning(f"More than one compatible model. Choosing {compatible_model_ids[0]} among {compatible_model_ids}")
+            compatible_multi_model_ids = [mid for mid in compatible_model_ids if mid.startswith(MULTIMODALCODE)]
+            model_id = config._pair_to_model_id(compatible_multi_model_ids[0])
+            if len(compatible_multi_model_ids) > 1:
+                logger.warning(f"More than one compatible model. Choosing {compatible_multi_model_ids[0]} among {compatible_multi_model_ids}")
 
         else:
             raise HTTPException(
@@ -66,7 +66,7 @@ def fetch_model_data_from_request(request):
             detail=f'No multilingual model support for pair {src}-{tgt}. Remove flag `use_multi` from request',
         )
 
-    if DEVDEBUG: logger.debug(f'model_id {model_id}')
+    logger.debug(f'model_id {model_id}')
 
     return model_id, src, tgt
 
